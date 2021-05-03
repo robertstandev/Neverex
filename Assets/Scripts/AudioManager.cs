c5c5013ae1485f1ba4c56ad2b3dec26e1dbfe279
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-05-03 18:58:09 +0300

    Add files via upload

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    public AudioSource backgroundMusic, tokenSound, scoreSound, deathSound;
+
+    [HideInInspector]
+    public bool soundIsOn = true;
+
+    public void StopBackgroundMusic()
+    {
+        backgroundMusic.Stop();
+    }
+
+    public void PlayBackgroundMusic()
+    {
+        if (soundIsOn)
+            backgroundMusic.Play();
+    }
+
+    public void TokenSound()
+    {
+        if(soundIsOn)
+            tokenSound.Play();
+    }
+
+    public void ScoreSound()
+    {
+        if (soundIsOn)
+            scoreSound.Play();
+    }
+
+    public void DeathSound()
+    {
+        if (soundIsOn)
+            deathSound.Play();
+    }
+}
