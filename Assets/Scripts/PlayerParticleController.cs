1541c2f95bd1456c90b6d78a5d5201cc140f4663
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-05-04 10:21:16 +0300

    Update PlayerParticleController.cs

--- Assets/Scripts/PlayerParticleController.cs
+++ Assets/Scripts/PlayerParticleController.cs
@@ -2,52 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerParticleController : MonoBehaviour {
-
+public class PlayerParticleController : MonoBehaviour
+{
     private ParticleSystem basicParticle, deathParticle;
 
-
-	private void Start () {
-        InitializeParticles();
+	private void Start ()
+    {
+        initializeParticles();
         basicParticle.Play();
 	}
 
-    private void InitializeParticles()
+    private void initializeParticles()
     {
         basicParticle = GameObject.FindGameObjectWithTag("BasicParticle").GetComponent<ParticleSystem>();
         deathParticle = GameObject.FindGameObjectWithTag("DeathParticle").GetComponent<ParticleSystem>();
     }
 
-    private void SetBasicParticleColor(GameObject obj)
+    private void setBasicParticleColor(GameObject obj)
     {
-        Color tempColor = GetColor(obj);
+        Color tempColor = getColor(obj);
         tempColor.a = 1f;
-        SetColor(basicParticle, tempColor);
+        setColor(basicParticle, tempColor);
     }
 
-    public void PlayDeathParticle()
+    public void playDeathParticle()
     {
-        SetColor(deathParticle, GetColor(basicParticle));
+        setColor(deathParticle, getColor(basicParticle));
         basicParticle.Stop();
         deathParticle.Play();
     }
 
-    private Color GetColor(GameObject obj)
+    private Color getColor(GameObject obj)
     {
         return obj.GetComponent<Renderer>().material.color;
     }
 
-    private Color GetColor(ParticleSystem obj)
+    private Color getColor(ParticleSystem obj)
     {
         return obj.GetComponent<Renderer>().material.color;
     }
 
-    private void SetColor(GameObject obj, Color color)
+    private void setColor(GameObject obj, Color color)
     {
         obj.GetComponent<Renderer>().material.color = color;
     }
 
-    private void SetColor(ParticleSystem obj, Color color)
+    private void setColor(ParticleSystem obj, Color color)
     {
         obj.GetComponent<Renderer>().material.color = color;
     }
