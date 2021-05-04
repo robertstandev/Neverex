a5fa92dbf08ea2659d424aeb2c05976ebd1b0142
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-05-04 10:21:52 +0300

    Update ScoreManager.cs

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -3,45 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class ScoreManager : MonoBehaviour {
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField]private TextMeshProUGUI scoreText, tokenText;
 
-    public TextMeshProUGUI scoreText, tokenText;
+    [SerializeField]private int score = 0;
 
-    [HideInInspector]
-    public int score = 0;
-
-    void Start()
+    private void Start()
     {
         tokenText.text = PlayerPrefs.GetInt("Token", 0).ToString();
     }
 
-    public void IncrementScore()
+    public void incrementScore()
     {
-        if (FindObjectOfType<Collision>().gameIsOver == false)
+        if (FindObjectOfType<Collision>().isGameOver() == false)
         {
             scoreText.text = (++score).ToString();
         }
     }
 
-    public void IncrementToken()
+    public void incrementToken()
     {
-        if (FindObjectOfType<Collision>().gameIsOver == false)
+        if (FindObjectOfType<Collision>().isGameOver() == false)
         {
             PlayerPrefs.SetInt("Token", PlayerPrefs.GetInt("Token", 0) + 1);
             tokenText.text = PlayerPrefs.GetInt("Token", 0).ToString();
         }
     }
 
-    public void IncrementToken(int countOfToken)
+    private void incrementToken(int countOfToken)
     {
         PlayerPrefs.SetInt("Token", PlayerPrefs.GetInt("Token", 0) + countOfToken);
         tokenText.text = PlayerPrefs.GetInt("Token", 0).ToString();
-        FindObjectOfType<AudioManager>().TokenSound();
+        FindObjectOfType<AudioManager>().playTokenSound();
     }
 
-    public void DecrementToken(int decreaseValue)
+    private void decrementToken(int decreaseValue)
     {
         PlayerPrefs.SetInt("Token", PlayerPrefs.GetInt("Token", 0) - decreaseValue);
         tokenText.text = PlayerPrefs.GetInt("Token", 0).ToString();
+    }
+
+    public int getScore()
+    {
+        return this.score;
     }
 }
