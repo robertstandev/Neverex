02a486b283910e9430bb771a6766b88a5077d1a3
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-05-09 10:48:52 +0300

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,32 +1,22 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]private float computerSpeed, movementSpeed;
-    private InputAction movementInput;
-    private float movementValue = 0;
+    [SerializeField]private float movementSpeed;
+    private IInput movementInput;
 
-    private void Awake() {
-        movementInput = GetComponent<IInput>().getMovementInput;
-        movementInput.performed += ctx => OnMove(ctx);
-        movementInput.canceled += ctx => OnMove(ctx);
-    }
-
-    private void OnEnable() { movementInput.Enable(); }
-    private void OnDisable() { movementInput.Disable(); }
-
-    private void OnMove(InputAction.CallbackContext context) { movementValue = context.ReadValue<float>(); }
+    private void Awake() { movementInput = GetComponent<IInput>(); }
 
     private void Start() { transform.GetChild(0).GetComponent<Animation>().Play(); }
 
     private void FixedUpdate()
     {
-        if(movementValue != 0)
+        if(movementInput.getMovementValue != 0)
         {
-            transform.RotateAround(Vector3.zero, transform.forward, (movementValue * computerSpeed) * movementSpeed * Time.deltaTime);
+            transform.RotateAround(Vector3.zero, transform.forward, movementInput.getMovementValue * movementSpeed * Time.deltaTime);
         }
     }
 }
