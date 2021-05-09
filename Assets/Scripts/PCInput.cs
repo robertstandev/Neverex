03d3fc3648cc6f47711202f3b713f114a85b5e07
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-05-09 10:48:23 +0300

    Update PCInput.cs

--- Assets/Scripts/PCInput.cs
+++ Assets/Scripts/PCInput.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -6,5 +6,17 @@ using UnityEngine.InputSystem;
 public class PCInput : MonoBehaviour, IInput
 {
     [SerializeField]private InputAction movementInput;
-    public InputAction getMovementInput { get { return this.movementInput; } }
+    private float movementValue = 0;
+
+    private void Awake() {
+        movementInput.performed += ctx => OnMove(ctx);
+        movementInput.canceled += ctx => OnMove(ctx);
+    }
+
+    private void OnEnable() { movementInput.Enable(); }
+    private void OnDisable() { movementInput.Disable(); }
+
+    private void OnMove(InputAction.CallbackContext context) { movementValue = context.ReadValue<float>(); }
+
+    public float getMovementValue { get { return this.movementValue; } }
 }
